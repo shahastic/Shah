2d2cc6dcfc114a20a4ae49e020581395e2e35b9a
shahastic <shahr6485@gmail.com>
2022-01-24 01:17:19 +0530

    th commit

--- hogya.php
+++ hogya.php
@@ -6,7 +6,7 @@ session_start();
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="index.css">
+    <!-- <link rel="stylesheet" href="index.css"> -->
     <script>
     </script>
     <title>Unsubscribed
@@ -16,14 +16,20 @@ session_start();
 <body>
 <?php
 $email = $_SESSION['email'];
+echo $email;
 $emailV = $_GET['email'];
+echo $emailV;
 if ($email == $emailV) {
     $indexPage = "https://shavi1111.herokuapp.com/index.php";
     include '_dbconnect.php';
     $sql = "UPDATE `shavi` SET `active` = '0' WHERE `shavi`.`email` = '$email'";
+    echo $sql;
     $sqldel = "DELETE FROM `shavi` WHERE `shavi`.`email` = '$email'";
+    echo $sqldel;
     $result = mysqli_query($conn, $sql);
+    echo $result;
     $resultDel = mysqli_query($conn, $sqldel);
+    echo $resultDel;
     session_unset();
     session_destroy();
     if ($result) {
