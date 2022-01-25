5fa58e34bdfb04904caeea71fbe0338a298be78c
shahastic <shahr6485@gmail.com>
2022-01-25 14:44:33 +0530

    nth commit

--- hogya.php
+++ hogya.php
@@ -7,12 +7,13 @@ include '_dbconnect.php';
 // $email = $_SESSION['email'];
 
 $emailV = $_GET['email'];
+$tokken = $_GET['token'];
 
-if ($email == $emailV) {
+if ($email == $emailV && $tokken == $token) {
     // $indexPage = "https://shavi1111.herokuapp.com/index.php";
-    $sql = "UPDATE `shavi` SET `active` = '0' WHERE `shavi`.`email` = '$email'";
+    $sql = "UPDATE `shavi` SET `active` = '0' WHERE `shavi`.`email` = '$email' AND `shavi` . `token` = '$token'";
     
-    $sqldel = "DELETE  FROM `shavi` WHERE `shavi`.`email` = '$email'";
+    $sqldel = "DELETE  FROM `shavi` WHERE `shavi`.`email` = '$email' AND `shavi` . `token` = '$token'";
     
     $result = mysqli_query($conn, $sql);
     
